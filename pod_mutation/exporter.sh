5becfb60988190117ac0a4b6c693904c1967eafa
Alex Christopher Ramos <alexchrisramos@Alex-Christopher-Ramos-MacBook-Pro.local>
2025-04-04 14:47:20 +0800

    feat(pod_mutation): added script to export mutation rules from 1 cluster going to multiple clusters

--- pod_mutation/exporter.sh
+++ pod_mutation/exporter.sh
@@ -1,55 +1,50 @@
 #!/bin/bash
-# Usage: ./exporter.sh [--dry-run]
+# Usage: ./exporter.sh
 set -euo pipefail
 
-dry_run=false
-if [ $1 == "--dry-run" ]; then
-    dry_run=true
-    shift
-fi
-
+DRY_RUN=true
 ORG_API_KEY=${ORG_API_KEY:-""}
-ORG_ID="1f1848fe-ac13-4e04-abc6-0ef2d578a49b"
-CLUSTERID_MUTATION_SRC="07bfd0a7-3f6d-4675-8dfb-335aadc979bb"
-CLUSTERID_MUTATION_DEST=("60f41cd7-2ae6-4745-8eb3-bbda88db29bc")
+ORG_ID="<ORGANIZATION_ID>"
+CLUSTERID_MUTATION_SRC="<CLUSTERID_OF_POD_MUTATION_SOURCE>"
+CLUSTERID_MUTATION_DEST=("<CLUSTERID_OF_POD_MUTATION_DESTINATION_01>" "<CLUSTERID_OF_POD_MUTATION_DESTINATION_02>")
+
 
 if [ -z "$ORG_API_KEY" ]; then
-    echo "ORG_API_KEY is not set. Exiting."
+    echo "Environment variable 'ORG_API_KEY' not set."
+    exit 1
+fi
+if [ -z "$ORG_ID" ]; then
+    echo "'ORG_ID' not set."
+    exit 1
+fi
+if [ -z "$CLUSTERID_MUTATION_SRC" ]; then
+    echo "'CLUSTERID_MUTATION_SRC' not set."
     exit 1
 fi
 
-get_pod_mutation() {
-    $(curl --request GET \
+
+# Get all pod mutation from source
+MUTATION_LIST=$(curl --request GET \
      --url https://api.cast.ai/patching-engine/v1beta/organizations/$ORG_ID/clusters/$CLUSTERID_MUTATION_SRC/pod-mutations \
      --header "X-API-Key: $ORG_API_KEY" \
      --header "accept: application/json")
-}
+MUTATION_LIST=$(echo "$MUTATION_LIST" | jq 'del(.items[].id)' | jq 'del(.items[].createTime)' | jq 'del(.items[].updateTime)' | jq 'del(.items[].clusterId)' | jq 'del(.items[].organizationId)')
 
-# create_pod_mutation() {
-#     curl --request POST \
-#      --url https://api.cast.ai/patching-engine/v1beta/organizations/%7Bpod_mutation.organization_id%7D/clusters/%7Bpod_mutation.cluster_id%7D/pod-mutations \
-#      --header 'X-API-Key: ' \
-#      --header 'accept: application/json' \
-#      --header 'content-type: application/json' \
-#      --data '
-# {
-#   "name": "test",
-#   "enabled": true
-# }
-# '
-# }
 
-# Get all pod mutation from source
-MUTATION_LIST=$(get_pod_mutation)
-
-if [ $dry_run == true ]; then
+for CLUSTERID in "${CLUSTERID_MUTATION_DEST[@]}"; do
     for item in $(echo "$MUTATION_LIST" | jq -c '.items[]'); do
-        echo "DYRUN: Pod mutation: $item"
+        if $DRY_RUN; then
+            echo "DRY_RUN"
+            echo "Applying pod mutation to cluster $CLUSTERID"
+            echo "$item" | jq -c '.'
+        else
+            echo "Applying pod mutation to cluster $CLUSTERID"
+            curl --request POST \
+            --url https://api.cast.ai/patching-engine/v1beta/organizations/%7B${ORG_ID}%7D/clusters/%7B${CLUSTERID}%7D/pod-mutations \
+            --header "X-API-Key: $ORG_API_KEY" \
+            --header 'accept: application/json' \
+            --header 'content-type: application/json' \
+            --data "$item"            
+        fi
     done
-fi
-
-if [ $dry_run == false ]; then
-    # TODO: Call POST API to create pod mutation
-fi
-
-
+done
